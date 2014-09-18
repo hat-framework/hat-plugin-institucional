3cb5bed4ce549708d191fb3a4be612ae586de686
Paulo Veneroso <venerosobrasil@gmail.com>
2014-09-18 09:24:46 -0300

    menu retirado

--- Config/institucionalConfigurations.php
+++ Config/institucionalConfigurations.php
@@ -3,12 +3,7 @@
 class institucionalConfigurations extends \classes\Classes\Options{
 
     protected $menu = array(
-        array(
-            'menuid' => 'Sobre',
-            'menu'   => 'Sobre',
-            'url'    => 'institucional/index/index',
-            'ordem'  => '3',
-        ),
+        array(),
     );
     
    
